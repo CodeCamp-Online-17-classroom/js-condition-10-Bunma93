fa4adba8bf5ce1c424b7c8eb76866979f582696d
Bunma93 <bunma93@gmail.com>
2024-06-28 10:33:10 +0700

    Js-condition10

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // write code here
+let userName = prompt("username");
+let passWord = prompt("password");
+if (!userName||!passWord) {
+    alert("username or password is required")
+} else if (userName == "admin" && passWord == "1234" 
+    || userName == "john" && passWord == "qwerty") {
+        alert(`Hello ${userName}`)
+    } else {
+        alert("invalid username or password")
+    };
